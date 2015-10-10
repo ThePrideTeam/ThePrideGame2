ffda12e049f0d59fb5d3d223c4c279bf66f1d0b7
the-archer <simratsinghchhabra@gmail.com>
2015-10-10 12:54:34 -0700

    added rainbow trail

--- rendergraphics.js
+++ rendergraphics.js
@@ -47,6 +47,10 @@ function rendergraphics_full(arr)
             {
                 img.src = "images/cop.jpg";
             }
+            else if(arr[i][j].charAt(0) == 'E')
+            {
+                img.src = "images/finish.jpg";
+            }
             cell.appendChild(img);
 
              row.appendChild(cell);
@@ -68,25 +72,71 @@ function rendergraphics_full(arr)
 
 function rendergraphics_partial(arr, i, j, oldi, oldj)
 {
-	//alert("here");
-	var cell = document.getElementById(i+","+j);
-	//alert(cell);
-	//alert("sir");
-	if (arr[i][j].charAt(0) == 'S')
-	{
-		//alert("here");
-		cell.getElementsByTagName('img')[0].src = "images/user.jpg";
-		
-	}
+    //alert("here");
+    var cell = document.getElementById(i+","+j);
+    //alert(cell);
+    //alert("sir");
+    if (arr[i][j].charAt(0) == 'S')
+    {
+        //alert("here");
+        cell.getElementsByTagName('img')[0].src = "images/user.jpg";
+        
+    }
 
-	var cell = document.getElementById(oldi+","+oldj);
-	//alert(cell);
-	//alert("sir");
-	if (arr[oldi][oldj].charAt(0) == 'T')
-	{
-		//alert("here");
-		cell.getElementsByTagName('img')[0].src = "images/roadC.png";
-		
-	}
+    var cell = document.getElementById(oldi+","+oldj);
+    //alert(cell);
+    //alert("sir");
+    if (arr[oldi][oldj].charAt(0) == 'T')
+    {
+        i = oldi
+        j = oldj
+        var img = cell.getElementsByTagName('img')[0]; 
+        //alert("here")l
+        var dir = [false,false,false,false];
+        if( i>0 && (arr[i-1][j].charAt(0)=='S' || arr[i-1][j]=='T'))
+        {
+            dir[0] = true;
+        }
+        if( j<arr[0].length-1 && (arr[i][j+1].charAt(0)=='S' || arr[i][j+1]=='T'))
+        {
+            dir[1] = true;
+        }
+        if( i<arr.length && (arr[i+1][j].charAt(0)=='S' || arr[i+1][j]=='T'))
+        {
+            dir[2] = true;
+        }
+        if( j>0 && (arr[i][j-1].charAt(0)=='S' || arr[i][j-1]=='T'))
+        {
+            dir[3] = true;
+        }
+        //alert(dir);
+        if(dir[0] && dir[2] && !dir[1] && !dir[3])
+        {
+            img.src = "images/rainbowV.gif";
+        }
+        else if(!dir[0] && dir[1] && !dir[2] && dir[3])
+        {
+            img.src = "images/rainbowH.gif";
+        }
+        else if(dir[0] && dir[1] && !dir[2] && !dir[3])
+        {
+            img.src = "images/rainbowLLC.png";
+        }
+        else if(!dir[0] && dir[1] && dir[2] && !dir[3])
+        {
+            img.src = "images/rainbowLUC.png";
+        }
+        else if(!dir[0] && !dir[1] && dir[2] && dir[3])
+        {
+            img.src = "images/rainbowRUC.png";
+        }
+        else if(dir[0] && !dir[1] && dir[2] && dir[3])
+        {
+            img.src = "images/rainbowRLC.png";
+        }
+        else
+            img.src = "images/roadC.png";
+        
+    }
 
 }
